fcde05a56d2f3136c067ac6238801076ad53c081
“youxiaobin” <y1006881983@qq.com>
2017-08-21 20:41:22 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import goods from './components/goods/goods.vue'
+import ratings from './components/ratings/ratings.vue'
+import seller from './components/seller/seller.vue'
+Vue.use(VueRouter)
+
+/* eslint-disable no-new */
+let app = Vue.extend(App)
+let router = new VueRouter({
+  linkActiveClass: 'active'
+})
+router.map({
+  '/goods': {
+    component: goods
+  },
+  '/ratings': {
+    component: ratings
+  },
+  '/seller': {
+    component: seller
+  }
+})
+router.start(app, '#app')
+// 默认跳转
+router.go('/goods')
